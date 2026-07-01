c80f97a46969e638a32b26e5d4a83945f728cb4c
Cesar Abdiel <cesarabdiel@gmail.com>
2026-07-01 18:50:32 -0400

    feat(productos): agregar CRUD básico de productos

--- src/productos.py
+++ src/productos.py
@@ -1,5 +1,20 @@
-class Producto:
-    def __init__(self, nombre, precio, stock):
-        self.nombre = nombre
-        self.precio = precio
-        self.stock = stock
+productos = []
+
+def registrar_producto(nombre, precio, stock):
+    producto = {
+        "nombre": nombre,
+        "precio": precio,
+        "stock": stock
+    }
+    productos.append(producto)
+    return producto
+
+def listar_productos():
+    return productos
+
+def actualizar_stock(nombre, nuevo_stock):
+    for producto in productos:
+        if producto["nombre"] == nombre:
+            producto["stock"] = nuevo_stock
+            return producto
+    return None
